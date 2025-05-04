63aed48de815510f7b8791e479520e73ebd52570
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2025-05-04 15:50:51 +0900

    chore: hello関数を編集2

--- hello.go
+++ hello.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+// 定数は、 Helloが呼び出されるたびに"Hello、"文字列インスタンスを作成する手間を省く
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
+	// return fmt.Sprintf("%s%s", englishHelloPrefix, name)
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// t.Helper()は、このメソッドがヘルパーであることをテストスイートに伝えるために必要です。
+		// こうすることで、テストが失敗したときに報告される行番号は、テストヘルパーの中ではなく 呼び出された関数 の中を示します。
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q != want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
