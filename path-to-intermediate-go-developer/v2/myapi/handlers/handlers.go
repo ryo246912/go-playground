a44d949c503d192d1aea5b7a8d997c7b2e50f390
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2025-05-13 23:58:39 +0900

    chore(intermediate): レスポンスをjsonで返すように変更

--- path-to-intermediate-go-developer/v2/myapi/handlers/handlers.go
+++ path-to-intermediate-go-developer/v2/myapi/handlers/handlers.go
@@ -1,19 +1,27 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/ryo246912/path-to-intermediate-go-developer/models"
 )
 
 func Hellohandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello World!\n")
 }
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "fail to encode json\n", http.StatusBadRequest)
+		return
+	}
+	w.Write(jsonData)
 }
 func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
@@ -29,6 +37,17 @@ func GetArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	} else {
 		page = 1
 	}
+
+	articles := []models.Article{
+		models.Article1,
+		models.Article2,
+	}
+	jsonData, err := json.Marshal(articles)
+	if err != nil {
+		http.Error(w, "Invaild query parameter", http.StatusBadRequest)
+		return
+	}
+	w.Write(jsonData)
 	io.WriteString(w, fmt.Sprintf("Article List (page %d)\n", page))
 }
 func GetArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
@@ -36,11 +55,30 @@ func GetArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		http.Error(w, "Invaild query parameter", http.StatusBadRequest)
 	}
-	io.WriteString(w, fmt.Sprintf("Article No.%d\n", articleID))
+
+	jsonData, err := json.Marshal(models.Article1)
+	if err != nil {
+		http.Error(w, "Invaild query parameter", http.StatusBadRequest)
+		return
+	}
+	w.Write(jsonData)
+	io.WriteString(w, fmt.Sprintf("Article No%d\n", articleID))
 }
 func PostNiceArticleHandler(w http.ResponseWriter, req *http.Request) {
+	jsonData, err := json.Marshal(models.Article1)
+	if err != nil {
+		http.Error(w, "Invaild query parameter", http.StatusBadRequest)
+		return
+	}
+	w.Write(jsonData)
 	io.WriteString(w, "Posting Nice...\n")
 }
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	jsonData, err := json.Marshal(models.Comment1)
+	if err != nil {
+		http.Error(w, "Invaild query parameter", http.StatusBadRequest)
+		return
+	}
+	w.Write(jsonData)
 	io.WriteString(w, "Posting Comment...\n")
 }
